767d81cf93ca2956ee6525af6b60d9c809534880
rajsaugata <rajsaugata@gmail.com>
2022-01-18 13:58:35 +0530

    Angular added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImgcomponentComponent } from './imgcomponent/imgcomponent.component';
+import { EmpInfoComponent } from './emp-info/emp-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextcomponentComponent,
+    ImgcomponentComponent,
+    EmpInfoComponent
   ],
   imports: [
     BrowserModule,
